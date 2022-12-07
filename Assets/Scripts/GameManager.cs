665abfdb66eed8cf8c531af43659398e1077f2ab
Itziar Mensa <itziar.mensa@estudiantat.upc.edu>
2022-12-07 09:47:22 +0100

    Video 5 completat

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,10 +4,39 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+
+    public static GameManager instance = null;                //Static instance of GameManager which allows it to be accessed by any other script.
+    private BoardManager boardScript;                        //Store a reference to our BoardManager which will set up the level.
+    private int level = 3;                                    //Current level number, expressed in game as "Day 1".
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        //Check if instance already exists
+        if (instance == null)
+
+            //if not, set instance to this
+            instance = this;
+
+        //If instance already exists and it's not this:
+        else if (instance != this)
+
+            //Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a GameManager.
+            Destroy(gameObject);
+
+        //Sets this to not be destroyed when reloading scene
+        DontDestroyOnLoad(gameObject);
+
+        //Get a component reference to the attached BoardManager script
+        boardScript = GetComponent<BoardManager>();
+
+        //Call the InitGame function to initialize the first level 
+        InitGame();
+    }
+
+    void InitGame()
+    {
+        boardScript.SetupScene(level);
     }
 
     // Update is called once per frame
